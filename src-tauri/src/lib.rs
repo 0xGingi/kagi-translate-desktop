5fbdeafe339e3dd2be294b1ee9243213373f0147
0xGingi <0xgingi@0xgingi.com>
2025-06-06 15:39:28 -0400

    v0.1.2 - remove tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,4 @@
-use tauri::{Emitter, Manager, Window};
+use tauri::{Emitter, Window};
 use tauri_plugin_updater::UpdaterExt;
 
 #[tauri::command]
@@ -18,23 +18,14 @@ async fn go_to_translate(window: Window) {
     }
 }
 
-#[tauri::command]
-async fn show_window(window: Window) {
-    if let Err(e) = window.show() {
-        eprintln!("Failed to show window: {}", e);
-    }
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_window_state::Builder::default().build())
-        .on_window_event(|window, event| match event {
-            tauri::WindowEvent::CloseRequested { api, .. } => {
-                window.hide().unwrap();
-                api.prevent_close();
+        .on_window_event(|_window, event| match event {
+            tauri::WindowEvent::CloseRequested { .. } => {
             }
             _ => {}
         })
@@ -46,51 +37,12 @@ pub fn run() {
                 }
             });
 
-            let quit = tauri::menu::MenuItemBuilder::with_id("quit", "Quit")
-                .accelerator("CmdOrCtrl+Q")
-                .build(app)
-                .unwrap();
-            let show = tauri::menu::MenuItemBuilder::with_id("show", "Show")
-                .build(app)
-                .unwrap();
-            let hide = tauri::menu::MenuItemBuilder::with_id("hide", "Hide")
-                .build(app)
-                .unwrap();
-
-            let menu = tauri::menu::MenuBuilder::new(app)
-                .items(&[&show, &hide, &quit])
-                .build()
-                .unwrap();
-
-            let _tray = tauri::tray::TrayIconBuilder::new()
-                .tooltip("Kagi Translate")
-                .menu(&menu)
-                .icon(app.default_window_icon().unwrap().clone())
-                .on_menu_event(|app, event| match event.id().0.as_str() {
-                    "quit" => app.exit(0),
-                    "show" => {
-                        if let Some(window) = app.get_webview_window("main") {
-                            window.show().unwrap();
-                            window.set_focus().unwrap();
-                        }
-                    }
-                    "hide" => {
-                        if let Some(window) = app.get_webview_window("main") {
-                            window.hide().unwrap();
-                        }
-                    }
-                    _ => {}
-                })
-                .build(app)
-                .unwrap();
-
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
             send_url,
             set_current_url,
-            go_to_translate,
-            show_window
+            go_to_translate
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
